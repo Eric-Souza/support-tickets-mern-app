19b4b9d78aebf54dee0de9ef6445367a4a702112
Eric Souza <eric.bh18souza@gmail.com>
2022-10-19 09:37:28 -0300

    feat: :sparkles: get and post note endpoints developed

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -10,6 +10,10 @@ const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
+// Re-route into note router
+const noteRouter = require("./noteRoutes");
+router.use("/:ticketId/notes", noteRouter);
+
 router.route("/").get(protect, getTickets).post(protect, createTicket);
 
 router
